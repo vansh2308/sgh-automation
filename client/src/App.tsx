4b31f57a543748ca4e2b7e73e9e202c0c095a0e8
VANSH AGARWAL <vanshagarwal11@gmail.com>
2024-09-26 03:53:55 +0530

    Init frontend

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+
+export default function App(){
+  return(
+    <div className="w-screen h-screen bg-blue-400 text-blue-800 text-[3rem] font-bold flex items-center justify-center">
+      Hare krishna
+
+    </div>
+  )
+}
